ec5a0d8e7b6898c037a22a2baa09bbe5d69388af
Roslaan001 <abdulsomad005@gmail.com>
2025-09-13 15:29:47 +0100

    edited the html and the docker compose file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let path = require("path");
 let fs = require("fs");
-let MongoClient = require("mongodb").MongoClient;
+let { MongoClient } = require("mongodb");
 let bodyParser = require("body-parser");
 
 let app = express();
@@ -9,43 +9,46 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// URLs
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb:27017";
+// Mongo connection URLs
+const mongoUrlLocal = "mongodb://admin:password@localhost:27017/admin";
+const mongoUrlDocker = "mongodb://admin:password@mongodb:27017/admin";
 
-// pick URL based on environment (inside Docker vs local)
-// if HOSTNAME is set, assume we are in Docker
-let mongoUrl = process.env.HOSTNAME ? mongoUrlDockerCompose : mongoUrlLocal;
+// Use environment variable if set, otherwise pick local vs docker
+const mongoUrl =
+  process.env.MONGO_URL ||
+  (process.env.DOCKER ? mongoUrlDocker : mongoUrlLocal);
 
 // Mongo options
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-// database & collection
-let databaseName = "user-account";
-let collectionName = "users";
+// Database & collection
+const databaseName = "user-account";
+const collectionName = "users";
 
 // serve homepage
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // serve a test image
-app.get("/profile-picture", function (req, res) {
+app.get("/profile-picture", (req, res) => {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, { "Content-Type": "image/jpg" });
   res.end(img, "binary");
 });
 
 // insert a record
-app.post("/api/add", async function (req, res) {
+app.post("/api/add", async (req, res) => {
   let client;
   try {
     client = await MongoClient.connect(mongoUrl, mongoClientOptions);
     let db = client.db(databaseName);
+
     let result = await db.collection(collectionName).insertOne({
       name: req.body.name,
       email: req.body.email,
     });
+
     res.json({ message: "User added successfully!", id: result.insertedId });
   } catch (err) {
     console.error("Insert error:", err);
@@ -56,11 +59,12 @@ app.post("/api/add", async function (req, res) {
 });
 
 // fetch all records
-app.get("/api/all", async function (req, res) {
+app.get("/api/all", async (req, res) => {
   let client;
   try {
     client = await MongoClient.connect(mongoUrl, mongoClientOptions);
     let db = client.db(databaseName);
+
     let users = await db.collection(collectionName).find({}).toArray();
     res.json(users);
   } catch (err) {
@@ -71,6 +75,8 @@ app.get("/api/all", async function (req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+// start server
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+  console.log("MongoDB URL:", mongoUrl);
 });
